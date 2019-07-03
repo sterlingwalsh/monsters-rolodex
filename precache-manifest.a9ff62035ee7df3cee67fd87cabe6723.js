c4bdb5923146399fb22084093fc360edf149f738
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-07-03 09:51:24 -0400

    Updates

---
+++ precache-manifest.a9ff62035ee7df3cee67fd87cabe6723.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84bdbd41e84a28a55afe041a999929bf",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "953afca3d3eeda8d5d2a",
+    "url": "/monsters-rolodex/static/css/main.842a2b19.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "953afca3d3eeda8d5d2a",
+    "url": "/monsters-rolodex/static/js/main.f2a719ba.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
